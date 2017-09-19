833bcdd4c61863811656f9f151c4d270c47a6fce
shin <funny.suo@gmail.com>
2017-09-19 19:14:48 +0900

    add IComparer

--- Program.cs
+++ Program.cs
@@ -2,6 +2,12 @@
 
 namespace test1
 {
+    public class Student
+    {
+        public int Age{get;set;}
+        public string Name{get;set;} 
+    }
+
     class Program
     {
         static void Main(string[] args)
@@ -48,6 +54,48 @@ namespace test1
             Console.WriteLine("");
 
             Console.WriteLine("aaHello World!,{0},{1}",numbersByRemainder.Rank,numbersByRemainder.Length);
+
+
+            int hashCode = numbers.GetHashCode();
+            Console.WriteLine("hashcode:{0},{1}",hashCode,numbers.ToString());
+            
+            int bins = System.Array.BinarySearch(numbers,3,8,3);
+
+            Console.WriteLine(bins);
+
+
+            Student[] students = new Student[4];
+            int i = 0;
+            int j = 1;
+            for (int index = 0; index < students.Length; index++)
+            {
+                Student item = new Student();
+                item.Name = i.ToString()+"abc";
+                item.Age = i*5*j;
+                students[index] = item;
+
+                i++;
+                j = -j;
+            }
+            foreach (var item in students)
+            {
+                item.Name = (i*j).ToString()+"abc";
+                item.Age = i*5*j;
+                i++;
+                j = -j;
+            }
+
+            foreach (var item in students)
+            {
+                Console.WriteLine("age:{0},name:{1}",item.Age,item.Name);
+            }
+            Array.Sort(students, (x,y)=>x.Age.CompareTo(y.Age));
+
+            foreach (var item in students)
+            {
+                Console.WriteLine("age:{0},name:{1}",item.Age,item.Name);
+            }
+
         }
     }
 }
